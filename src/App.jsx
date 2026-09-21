89b4667068a9508e8acfb38f811ab857d076b0a4
gwijigramsupport <gwijigram.support@gmail.com>
2026-09-21 23:29:26 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
 
 function App() {
   return (
     <div>
       <Navbar />
-
-      <h1>AI Characters</h1>
+      <Home />
     </div>
   )
 }
