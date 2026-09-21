c0bd800f2f30ee6bc9b4ca6c91a87e68605fe649
gwijigramsupport <gwijigram.support@gmail.com>
2026-09-22 00:04:59 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import CharacterCard from '../components/CharacterCard'
+
 function Home() {
   return (
     <main>
       <h1>Discover Characters</h1>
+
+      <CharacterCard />
     </main>
   )
 }
