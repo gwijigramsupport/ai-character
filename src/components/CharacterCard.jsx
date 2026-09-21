9f415411449cbe41337e504a05cb3f9642d9d593
gwijigramsupport <gwijigram.support@gmail.com>
2026-09-22 00:04:01 +0300

    Create src/components/CharacterCard.jsx

---
+++ src/components/CharacterCard.jsx
@@ -0,0 +1,16 @@
+function CharacterCard() {
+  return (
+    <article>
+      <div>
+        Character Image
+      </div>
+
+      <h2>Sarah</h2>
+      <p>Age: 24</p>
+      <p>⭐ 4.8</p>
+      <p>Photographer</p>
+    </article>
+  )
+}
+
+export default CharacterCard
