0d3ea8e4537477101ef8328f78e2b80895270cfd
gwijigramsupport <gwijigram.support@gmail.com>
2026-09-22 00:23:39 +0300

    Update CharacterCard.jsx

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,14 +1,21 @@
 function CharacterCard({ character }) {
   return (
-    <article>
-      <div>
-        Character Image
-      </div>
+    <article className="character-card">
+      <img
+        src={character.image}
+        alt={character.name}
+        className="character-image"
+      />
 
-      <h2>{character.name}</h2>
-      <p>Age: {character.age}</p>
-      <p>⭐ {character.rating}</p>
-      <p>{character.occupation}</p>
+      <div className="character-overlay"></div>
+
+      <div className="character-info">
+        <h2>{character.name}</h2>
+
+        <span className="character-age">
+          {character.age} yrs
+        </span>
+      </div>
     </article>
   )
 }
