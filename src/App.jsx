4095fcf86e7bf05eb480efff630c6d573767abea
gwijigramsupport <gwijigram.support@gmail.com>
2026-09-22 00:56:41 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import './App.css'
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Navbar />
-      <Home />
-    </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
