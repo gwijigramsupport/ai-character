b8759bef12099bc5d213482a7813da6f3c3fdd89
gwijigramsupport <gwijigram.support@gmail.com>
2026-09-22 00:08:46 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,37 @@
 import CharacterCard from '../components/CharacterCard'
 
 function Home() {
+  const characters = [
+    {
+      name: 'Sarah',
+      age: 24,
+      rating: 4.8,
+      occupation: 'Photographer',
+    },
+    {
+      name: 'Emily',
+      age: 27,
+      rating: 4.6,
+      occupation: 'Designer',
+    },
+    {
+      name: 'Aisha',
+      age: 25,
+      rating: 4.9,
+      occupation: 'Writer',
+    },
+  ]
+
   return (
     <main>
       <h1>Discover Characters</h1>
 
-      <CharacterCard />
+      {characters.map((character) => (
+        <CharacterCard
+          key={character.name}
+          character={character}
+        />
+      ))}
     </main>
   )
 }
