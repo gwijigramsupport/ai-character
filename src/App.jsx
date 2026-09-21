3d8706fbe56767aa59d78b4a297d33710042e92a
gwijigramsupport <gwijigram.support@gmail.com>
2026-09-22 01:00:58 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import CharacterProfile from './pages/CharacterProfile'
 import './App.css'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+      <Route path="/character/:id" element={<CharacterProfile />} />
       </Routes>
     </BrowserRouter>
   )
