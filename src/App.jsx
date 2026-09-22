8fcfb3ece0f4861368438b5109bf98623e399961
gwijigramsupport <gwijigram.support@gmail.com>
2026-09-23 00:23:25 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import { useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { supabase } from './supabase'
+
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import CharacterProfile from './pages/CharacterProfile'
 import Chat from './pages/Chat'
+
 import './App.css'
 
 function App() {
+  useEffect(() => {
+    async function testSupabase() {
+      const { data, error } = await supabase
+        .from('characters')
+        .select('name')
+
+      console.log('Supabase characters:', data)
+      console.log('Supabase error:', error)
+    }
+
+    testSupabase()
+  }, [])
+
   return (
     <BrowserRouter>
       <Navbar />
