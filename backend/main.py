f755e7e55b49db3bcabaa365f672ce5396955223
gwijigramsupport <gwijigram.support@gmail.com>
2026-09-22 13:22:36 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
+
 app = FastAPI()
+
+
+# Allow the React frontend to communicate with the Python backend
+# We are using "*" temporarily during development.
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class ChatRequest(BaseModel):
